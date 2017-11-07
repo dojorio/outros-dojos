8d2cc7a3082aee1860133e8ebc27b8b26fc9c70e
Vitor Mattos <vitor.mattos@phprio.org>
2017-11-07 19:58:38 -0200

    Piloto: Romulo Pires e Copiloto: Manoel França

--- 2017-11-07_fizz_buzz/ProblemTest.php
+++ 2017-11-07_fizz_buzz/ProblemTest.php
@@ -5,5 +5,11 @@ require_once 'vendor/autoload.php';
 
 class ProblemTest extends TestCase
 {
-    
+    public function testNumeroDeveSerDivisivelPor3() 
+    {
+        require_once 'Problem.php';
+        $numero = 1;
+        $problem = new Problem();
+        $this->assertTrue($problem->dividePor($numero, 3));
+    }
 }
