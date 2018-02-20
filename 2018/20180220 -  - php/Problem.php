cc8f3d85a0f9180d64847d8ef942c3e4c0c60d94
Vitor Mattos <vitor.mattos@phprio.org>
2018-02-20 20:17:29 -0300

    Piloto: Claudioney Loureiro e Copiloto: Daiane Alves

--- 2018/20180220 -  - php/Problem.php
+++ 2018/20180220 -  - php/Problem.php
@@ -2,4 +2,13 @@
 class Problem
 {
     public $valor;
+    
+    public function valorInformado()
+    {
+        if (!is_numeric($this->valor)) {
+            return;
+        }
+        return $this->valor;
+    }
+    
 }
