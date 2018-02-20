7dc20fc28b8afd6306c88e223e8bcd77fa010640
Vitor Mattos <vitor.mattos@phprio.org>
2018-02-20 20:37:02 -0300

    Piloto: Leonardo Gomes e Copiloto: yasmim lima

--- 2018/20180220 -  - php/tests/ProblemTest.php
+++ 2018/20180220 -  - php/tests/ProblemTest.php
@@ -15,34 +15,53 @@ require dirname(__FILE__) . '/../Problem.php';
 
 class ProblemTest extends TestCase
 {
-    function testVerificarSeExisteValor(){
+    function testVerificarSeExisteValor()
+    {
         $model = new Problem();
         $this->assertEmpty($model->valor);
     }
     
-    function testVerificarSeValorEUmNumero(){
+    function testVerificarSeValorEUmNumero()
+    {
         $model = new Problem();
         $model->valor = 50;
         $this->assertInternalType('integer', $model->valor);
     }
     
-    function testVerificarSeValorNaoEUmNumero(){
+    function testVerificarSeValorNaoEUmNumero()
+    {
         $model = new Problem();
         $model->valor = 'abacate';
         $this->assertEmpty($model->valorInformado());
     }
-    function testVerificarSeValorEhMenorQueDez(){
+    function testVerificarSeValorEhMenorQueDez()
+    {    
         $model = new Problem();
         $model->valor = 9;
         $this->assertEmpty($model->valorInformado());
         
     }
     
-    function testVerificarSeValorNaoEhDivisivelPorDez(){
+    function testVerificarSeValorNaoEhDivisivelPorDez()
+    {
         $model = new Problem();
         $model->valor = 12;
         $this->assertEmpty($model->valorInformado());
         
+    }
+    
+    function testVerificarSeValorEhDivisivelPorDez()
+    {
+        $model = new Problem();
+        $model->valor = 20;
+        $this->assertEquals(20, $model->valorInformado());
+    }
+    
+    function testVerificarSeValorEhMaiorOuIgualADez()
+    {
+        $model = new Problem();
+        $model->valor = 9;
+        $this->assertEquals(10, $model->valorInformado(), "vc errou");
     }
     
 }
