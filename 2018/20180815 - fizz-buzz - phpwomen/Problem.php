7b9fb1178768fbb928f1ab10d1a062cde4dc390e
Vitor Mattos <vitor.mattos@phprio.org>
2018-08-15 20:34:37 -0300

    Piloto: Perla e Copiloto: Luiz

--- 2018/20180815 - fizz-buzz - phpwomen/Problem.php
+++ 2018/20180815 - fizz-buzz - phpwomen/Problem.php
@@ -5,14 +5,20 @@ class Problem
     {
         $arr=array();
         for ($a=1; $a<=100; $a++){
+            if( $a % 3 == 0){
+                $a = "Fizz";
+                
+            }
            
           array_push($arr, $a);  
           
           
         } 
         
-        
-        $arr[2]="Fizz";
+       
+            
+            
+       
         $arr[4]="Buzz";
         $arr[14]="FizzBuzz";
         return $arr; 
